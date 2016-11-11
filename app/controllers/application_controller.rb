eda9c0d33c6c25e8be585be64313cbc1faa54383
Sullivan <sullivan1982@gmail.com>
2016-11-12 06:26:54 +0800

    add CROS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  rescue_from CanCan::AccessDenied do |exception|
-    puts '111111111111111111111111111111111'
-    #redirect_to main_app.root_path, :alert => exception.message
+
+  before_action :cors_set_access_control_headers
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
   end
 end
